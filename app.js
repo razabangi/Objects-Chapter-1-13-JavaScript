5a80b873377bf4ac1243b9998066b0c4ee8b8485
Muhammadrazabangi09 <muhammad.raza@cooperativecomputing.com>
2021-11-01 11:59:15 +0500

    chapter 3 complete chapter 4 in progress

---
+++ app.js
@@ -0,0 +1,123 @@
+// Chapter No 1 start
+
+// Answer No 1:
+// alert("Hello World!");
+
+// Answer No 2:
+// alert("Error! Please Enter a valid password.");
+
+// Answer No 3:
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Answer No 4:
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Answer No 5:
+// Done
+
+// Answer No 6:
+// Done
+
+// Answer No 7:
+// Done
+
+// Chapter No 1 End
+
+// -------------------------
+
+// Chapter No 2 Start
+
+// Answer No 1:
+// var username;
+
+// Answer No 2:
+// var myName = "Muhammad Raza Bangi";
+
+// Answer No 3:
+// var message = "Hello World!";
+// alert(message);
+
+// Answer No 4:
+// var stdName = "Muhammad Raza Bangi";
+// var stdAge = 23;
+// var stdCourse = "Mern Development";
+// alert(stdName);
+// alert(stdAge);
+// alert(stdCourse);
+
+// Answer No 5:
+// var str = "Pizza";
+// var newStrArr = [];
+// for (var i = str.length; i >= 1; i--) {
+//     newStrArr.push(str.slice(0, i));
+// }
+// var newStr = newStrArr.join(",");
+// var result = newStr.replace(/,/g, '\n');
+// alert(result);
+
+// Answer No 6:
+// var email = "muhammadrazabangi9@gmail.com";
+// var message = "My email address is: ";
+// alert(`${message} ${email}`);
+
+// Answer No 7:
+// var book = "A smarter way to learn javascript";
+// var message = "I am trying to learn from the book";
+// alert(`${message} ${book}`);
+
+// Answer No 8:
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Answer No 9:
+// var pattern = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(pattern);
+
+// Chapter No 2 End
+
+// -------------------------
+
+// Chapter No 3 Start
+
+// Answer No 1:
+// var age = 23;
+// alert(`I am ${age} year old.`);
+
+// Answer No 2:
+// var visitors = 0;
+// visitors++;
+// alert(`You have visited this site ${visitors} times.`);
+
+// Answer No 3:
+// var birthYear = 1997;
+// document.write(`My birth year is ${birthYear} <br>`);
+// document.write(`Data type of my declared variable is ${typeof birthYear}`);
+
+// Answer No 4:
+// var storeName = "www.daraz.pk";
+// var visitorName = "Muhammad Raza Bangi";
+// var productTitle = "Iphone 12 pro max black with official warranty";
+// var quantity = 5;
+// document.write(`${visitorName} ordered ${quantity} ${productTitle} on ${storeName}`);
+
+// Chapter No 3 End
+
+// -------------------------
+
+// Chapter No 4 Start
+
+// Answer No 1:
+// var name, age, designation;
+
+// Answer No 2:
+// Five Legal Variables:
+// var legalVariables = "Bangi"; 
+// var legal_variables = "Bangi"; 
+// var _legal_variables = "Bangi"; 
+// var legalVar123 = "Bangi"; 
+// var legalvariable = "Bangi"; 
+
+
+// Chapter No 4 End
+
+
